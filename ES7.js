fefaca78d2ff7a078d2c52398638e5678731b31a
RMuhamar <rmuhamar@gmail.com>
2018-11-18 16:35:51 +0700

    ES7

---
+++ ES7.js
@@ -0,0 +1,8 @@
+var ar = ['ray','muh','bangjags'];
+include('bangjags');
+
+ar.filter(name => name.includes("bang"));
+
+//exercise
+const arr = ['pro','gram','merit','program','it','programmer'];
+arr.filter(name => name.includes('pro'));
